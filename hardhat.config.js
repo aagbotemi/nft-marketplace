504fa08df64457c0daab2d092a54af2369c87dc5
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-12-05 06:18:01 +0100

    deploy function for creating nft

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomiclabs/hardhat-waffle")
+const fs = require('fs')
+const privateKey = fs.readFileSync(".env").toString().trim() || "01234567890123456789"
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -7,8 +9,8 @@ module.exports = {
       chainId: 1337
     },
     rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${process.env.PRIVATE_PROJECT_ID}`,
-      accounts: [process.env.PRIVATE_ACCOUNT_KEY]
+      url: `https://rinkeby.infura.io/v3/262c982def2340dcb7434a9dfa0e33c5`,
+      accounts: [privateKey]
     },
   },
   solidity: {
