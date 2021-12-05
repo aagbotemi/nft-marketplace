5c9336b8e2eda8f4163eef6573d8aa8283976a3a
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-12-05 05:59:51 +0100

    function to create item on the marketplace

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -46,5 +46,40 @@ contract NFTMarket is ReentrancyGuard {
     return listingPrice;
   }
 
+  /* Places an item for sale on the marketplace */
+  function createMarketItem(
+    address nftContract,
+    uint256 tokenId,
+    uint256 price
+  ) public payable nonReentrant {
+    require(price > 0, "Price must be at least 1 wei");
+    require(msg.value == listingPrice, "Price must be equal to listing price");
+
+    _itemIds.increment();
+    uint256 itemId = _itemIds.current();
+
+    idToMarketItem[itemId] =  MarketItem(
+      itemId,
+      nftContract,
+      tokenId,
+      payable(msg.sender),
+      payable(address(0)),
+      price,
+      false
+    );
+
+    IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
+
+    emit MarketItemCreated(
+      itemId,
+      nftContract,
+      tokenId,
+      msg.sender,
+      address(0),
+      price,
+      false
+    );
+  }
+
   
 }
