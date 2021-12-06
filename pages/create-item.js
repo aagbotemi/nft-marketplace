939568eddf42ecc392cf09d455bf1644678e1f47
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-12-06 14:45:38 +0100

    function to place the nft in the marketplace

--- pages/create-item.js
+++ pages/create-item.js
@@ -51,6 +51,32 @@ export default function CreateItem() {
         }  
     }
 
+    async function createSale(url) {
+        const web3Modal = new Web3Modal()
+        const connection = await web3Modal.connect()
+        const provider = new ethers.providers.Web3Provider(connection)    
+        const signer = provider.getSigner()
+
+        /* next, create the item */
+        let contract = new ethers.Contract(nftaddress, NFT.abi, signer)
+        let transaction = await contract.createToken(url)
+        let tx = await transaction.wait()
+        let event = tx.events[0]
+        let value = event.args[2]
+        let tokenId = value.toNumber()
+        const price = ethers.utils.parseUnits(formInput.price, 'ether')
+
+        /* then list the item for sale on the marketplace */
+        contract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
+        let listingPrice = await contract.getListingPrice()
+        listingPrice = listingPrice.toString()
+
+        transaction = await contract.createMarketItem(nftaddress, tokenId, price, { value: listingPrice })
+        await transaction.wait()
+        router.push('/')
+    }
+
+
     return (
         <div className="flex justify-center">
             Hello World
