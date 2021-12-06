369858fce8e65c763f7e6299f7a5c90205dd9d88
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-12-06 15:08:02 +0100

    UI of NFT in the gallery component

--- pages/my-assets.js
+++ pages/my-assets.js
@@ -16,6 +16,8 @@ export default function MyAssets() {
     useEffect(() => {
         loadNFTs()
     }, [])
+
+    console.log(nfts)
     async function loadNFTs() {
         /* const web3Modal = new Web3Modal({
         network: "mainnet",
@@ -50,7 +52,22 @@ export default function MyAssets() {
     
     return (
         <div className="flex justify-center">
-            Hello Gallery
+            <div className="p-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+
+                {
+                    nfts.map((nft, i) => (
+                        <div key={i} className="border shadow rounded-xl overflow-hidden">
+                            <img src={nft.image} className="rounded" />
+                            <div className="p-4 bg-black">
+                            <p className="text-2xl font-bold text-white">Price - {nft.price} Eth</p>
+                            </div>
+                        </div>
+                    ))
+                }
+                </div>
+            </div>
+            
         </div>
     )
 }
