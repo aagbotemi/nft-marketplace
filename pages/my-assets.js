6e5d1c1dda00390477071ee444df442740327830
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-12-06 15:02:28 +0100

    load NFT on the gallery page

---
+++ pages/my-assets.js
@@ -0,0 +1,56 @@
+import { ethers } from 'ethers'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Web3Modal from "web3modal"
+
+import {
+  nftmarketaddress, nftaddress
+} from '../config'
+
+import Market from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json'
+import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
+
+export default function MyAssets() {
+    const [nfts, setNfts] = useState([])
+    const [loadingState, setLoadingState] = useState('not-loaded')
+    useEffect(() => {
+        loadNFTs()
+    }, [])
+    async function loadNFTs() {
+        /* const web3Modal = new Web3Modal({
+        network: "mainnet",
+        cacheProvider: true,
+        }) */
+        const web3Modal = new Web3Modal()
+        const connection = await web3Modal.connect()
+        const provider = new ethers.providers.Web3Provider(connection)
+        const signer = provider.getSigner()
+
+        const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
+        const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
+        const data = await marketContract.fetchMyNFTs()
+
+        const items = await Promise.all(data.map(async i => {
+        const tokenUri = await tokenContract.tokenURI(i.tokenId)
+        const meta = await axios.get(tokenUri)
+        let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
+        let item = {
+            price,
+            tokenId: i.tokenId.toNumber(),
+            seller: i.seller,
+            owner: i.owner,
+            image: meta.data.image,
+        }
+        return item
+        }))
+        setNfts(items)
+        setLoadingState('loaded') 
+    }
+    
+    
+    return (
+        <div className="flex justify-center">
+            Hello Gallery
+        </div>
+    )
+}
