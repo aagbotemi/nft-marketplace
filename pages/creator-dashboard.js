c97c73379370bd4545deea41e4d41bf00386f708
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-12-06 15:18:09 +0100

    function to load NFT created by users

---
+++ pages/creator-dashboard.js
@@ -0,0 +1,60 @@
+import { ethers } from 'ethers'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Web3Modal from "web3modal"
+
+import {
+  nftmarketaddress, nftaddress
+} from '../config'
+
+import Market from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json'
+import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
+
+export default function CreatorDashboard() {
+    const [nfts, setNfts] = useState([])
+    const [sold, setSold] = useState([])
+    const [loadingState, setLoadingState] = useState('not-loaded')
+    useEffect(() => {
+        loadNFTs()
+    }, [])
+    async function loadNFTs() {
+        /* const web3Modal = new Web3Modal({
+        network: "mainnet",
+        cacheProvider: true,
+        }) */
+        const web3Modal = new Web3Modal()
+        const connection = await web3Modal.connect()
+        const provider = new ethers.providers.Web3Provider(connection)
+        const signer = provider.getSigner()
+
+        const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
+        const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
+        const data = await marketContract.fetchItemsCreated()
+
+        const items = await Promise.all(data.map(async i => {
+            const tokenUri = await tokenContract.tokenURI(i.tokenId)
+            const meta = await axios.get(tokenUri)
+            let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
+            let item = {
+                price,
+                tokenId: i.tokenId.toNumber(),
+                seller: i.seller,
+                owner: i.owner,
+                sold: i.sold,
+                image: meta.data.image,
+            }
+            return item
+        }))
+        /* create a filtered array of items that have been sold */
+        const soldItems = items.filter(i => i.sold)
+        setSold(soldItems)
+        setNfts(items)
+        setLoadingState('loaded') 
+    }
+
+    return (
+        <div>
+            Hello World
+        </div>
+    )
+}
