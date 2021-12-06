7136f995cba4f387ea22acc0c6f2792cfa9f74e3
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-12-06 15:00:23 +0100

    UI to create a NFT item

--- pages/create-item.js
+++ pages/create-item.js
@@ -78,8 +78,44 @@ export default function CreateItem() {
 
 
     return (
-        <div className="flex justify-center">
-            Hello World
+        <div className="">
+            <div className='text-center mt-8'>
+                <h1 className="font-bold text-3xl">Create NFT</h1>
+                <p className="">Mint an NFT and list it on the marketplace</p>
+            </div>
+            <div className='flex justify-center'>
+                <div className="w-1/2 flex flex-col pb-12">
+                    <input 
+                        placeholder="Asset Name"
+                        className="mt-8 border rounded p-4"
+                        onChange={e => updateFormInput({ ...formInput, name: e.target.value })}
+                    />
+                    <textarea
+                        placeholder="Asset Description"
+                        className="mt-2 border rounded p-4"
+                        onChange={e => updateFormInput({ ...formInput, description: e.target.value })}
+                    />
+                    <input
+                        placeholder="Asset Price in Eth"
+                        className="mt-2 border rounded p-4"
+                        onChange={e => updateFormInput({ ...formInput, price: e.target.value })}
+                    />
+                    <input
+                        type="file"
+                        name="Asset"
+                        className="my-4"
+                        onChange={onChange}
+                    />
+                    {
+                    fileUrl && (
+                        <img className="rounded mt-4" width="350" src={fileUrl} />
+                    )
+                    }
+                    <button onClick={createMarket} className="font-bold mt-4 bg-blue-light text-white rounded p-4 shadow-lg">
+                    Create Digital Asset
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
