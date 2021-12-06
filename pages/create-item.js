6e9f6bdb292c606de394169cce96687d447d4549
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-12-06 14:41:29 +0100

    creator setting file to ipfs

---
+++ pages/create-item.js
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import { ethers } from 'ethers'
+import { create as ipfsHttpClient } from 'ipfs-http-client'
+import { useRouter } from 'next/router'
+import Web3Modal from 'web3modal'
+
+const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
+
+import {
+  nftaddress, nftmarketaddress
+} from '../config'
+
+import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
+import Market from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json'
+
+export default function CreateItem() {
+    const [fileUrl, setFileUrl] = useState(null)
+    const [formInput, updateFormInput] = useState({ price: '', name: '', description: '' })
+    const router = useRouter()
+
+    async function onChange(e) {
+        const file = e.target.files[0]
+        try {
+            const added = await client.add(
+                file,
+                {
+                progress: (prog) => console.log(`received: ${prog}`)
+                }
+            )
+            const url = `https://ipfs.infura.io/ipfs/${added.path}`
+            setFileUrl(url)
+        } catch (error) {
+            console.log('Error uploading file: ', error)
+        }  
+    }
+    
+    return (
+        <div className="flex justify-center">
+            Hello World
+        </div>
+    )
+}
