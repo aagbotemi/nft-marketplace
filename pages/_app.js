666a31e6ab4d38f6433037efdcccf2ac446ac463
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-12-07 08:06:13 +0100

    connect wallet authentication and routing protection

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import '../styles/globals.css'
 import { Disclosure } from '@headlessui/react'
@@ -15,7 +15,63 @@ function classNames(...classes) {
 
 
 function MyApp({ Component, pageProps }) {
-  const currentAccount = "0x3904809348afd930092"
+
+  // const currentAccount = "0x3904809348afd930092"
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Make sure you have metamask!");
+        return;
+      } else {
+        alert("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        alert("Found an authorized account:", account);
+        setCurrentAccount(account);
+      } else {
+        alert("No authorized account found")
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  /**
+   * Implement your connectWallet method here
+   */
+  const connectWallet = async () => {
+    // setLoadingWallet(true);
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        // alert("Get MetaMask!");
+        // setLoadingWallet(false);
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+      // setLoadingWallet(false);
+    } catch (error) {
+      console.log(error)
+      // setLoadingWallet(false);
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
 
   return (
     <Disclosure as="nav">
@@ -41,15 +97,15 @@ function MyApp({ Component, pageProps }) {
                     src={Logo}
                     alt="Workflow"
                   /> */}
-                  <Link
+                  {currentAccount && (<Link
                     href="/my-assets"
                   >
                     <a className={'px-4 rounded-full font-base hidden sm:block'}>Gallery</a>
-                  </Link>
+                  </Link>)}
                 </div>
                 <div className="hidden md:block sm:ml-6">
                   <div className="flex space-x-4">
-                    {navigation.map((item) => (
+                    {currentAccount && navigation.map((item) => (
                       <Link
                         key={item.name}
                         href={item.url}
@@ -63,19 +119,32 @@ function MyApp({ Component, pageProps }) {
                       </Link>
                     ))}
 
-                    <div className='mt-2 text-blue-light'>Account: [{currentAccount.substring(0, 2).concat('...')}{currentAccount.substring(15)}]
-                    </div>
+                    {!currentAccount && (
+                      <button className="font-base cursor-pointer text-white bg-blue-light px-4 py-2 rounded-full text-sm font-medium" onClick={connectWallet}>
+                        Connect Wallet
+                      </button>
+                    )}
+
+                    {currentAccount && (
+                      <div className='mt-2 text-blue-light'>Account: [{currentAccount.substring(0, 2).concat('...')}{currentAccount.substring(currentAccount.length - 6)}]
+                      </div>
+                    )}
                   </div>
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center md:hidden">
+                {!currentAccount && (
+                  <button className="font-base cursor-pointer text-white text-blue-light border border-blue-light px-3 py-2 rounded-full text-sm font-medium" onClick={connectWallet}>
+                    Connect Wallet
+                  </button>
+                )}
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md focus:outline-none text-blue-light">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
                   ) : (
-                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                    currentAccount && (<MenuIcon className="block h-6 w-6" aria-hidden="true" />)
                   )}
                 </Disclosure.Button>
               </div>
@@ -83,7 +152,7 @@ function MyApp({ Component, pageProps }) {
           </div>
 
           <Disclosure.Panel className="md:hidden bg-blue-light text-white border-t">
-            <div className="px-2 pt-2 pb-3 space-y-1">
+            {currentAccount && (<div className="px-2 pt-2 pb-3 space-y-1">
               {navigation.map((item) => (
                 <Disclosure.Button
                   key={item.name}
@@ -99,11 +168,11 @@ function MyApp({ Component, pageProps }) {
                 </Disclosure.Button>
               ))}
               <Link
-                    href="/my-assets"
-                  >
-                    <a className={'block px-3 py-2 rounded-md text-sm font-medium sm:hidden'}>Gallery</a>
-                  </Link>
-            </div>
+                href="/my-assets"
+              >
+                <a className={'block px-3 py-2 rounded-md text-sm font-medium sm:hidden'}>Gallery</a>
+              </Link>
+            </div>)}
           </Disclosure.Panel>
           <Component {...pageProps} />
         </>
